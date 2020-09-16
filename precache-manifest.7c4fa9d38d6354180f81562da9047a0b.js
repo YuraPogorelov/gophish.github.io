8999ee7096c1a926a2a3a3b5bbee3b519b27219b
Yura Pogorelov <pyurik44@gmail.com>
2020-09-16 13:55:33 +0300

    Updates

---
+++ precache-manifest.7c4fa9d38d6354180f81562da9047a0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447e69b1c2c1c8e88a4e19016f7e6435",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1007f5c391c0a7db176",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "095fde412a48f0b1cb47",
+    "url": "/static/js/2.2bccb882.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2bccb882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1007f5c391c0a7db176",
+    "url": "/static/js/main.6530029a.chunk.js"
+  },
+  {
+    "revision": "354269fdb88608fa41da",
+    "url": "/static/js/runtime-main.59e07dcc.js"
+  }
+]);
